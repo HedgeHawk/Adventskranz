c6033e42405e7ffce041e2694da24525264dc800
HedgeHawk <darth-sidious@gmx.net>
2022-01-04 13:27:33 +0100

    Library

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//Build: g++ main.cpp -o advent
+//Test:  ./advent
+
+#include "advent.h"
+#include<iostream> 
+#include<iomanip>
+using namespace std; 
+
+void printCandles(int dy, int mo){
+  int a = adventstag(dy, mo);
+  cout << mo << " " <<std::setfill('0') << std::setw(2)<< dy << " " <<std::setfill('0') << std::setw(2)<< a <<" ";
+  for(int dow = 1; dow <= 7; dow++){
+    cout << candles(a,dow);
+  }
+  cout << endl;
+}
+
+int main () 
+{
+  cout << "mo dy  a 1234567"<<endl;
+  
+  //November
+  int mo = 11;
+  for(int dy=27;dy<=30;dy++){
+    printCandles(dy,mo);
+  }
+  
+  //Dezember
+  mo=12;
+  for(int dy=1;dy<=24;dy++){
+    printCandles(dy,mo);
+  }
+	
+  return 0; 
+} 
