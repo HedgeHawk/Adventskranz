38c9ed1468a797d28283819e90950d681d8d975c
HedgeHawk <darth-sidious@gmx.net>
2022-01-04 13:18:20 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "advent.h"
+#include<iostream> 
+#include<iomanip>
+using namespace std; 
+
+void printCandles(int dy, int mo){
+  int a = adventstag(dy, mo);
+  cout << mo << " " <<std::setfill('0') << std::setw(2)<< dy << " " <<std::setfill('0') << std::setw(2)<< a <<" ";
+	for(int dow = 1; dow <= 7; dow++){
+		cout << candles(a,dow);
+	}
+}
+
+int main () 
+{
+  cout << "mo dy  a 1234567"<<endl;
+  
+  //November
+  int mo = 11;
+  for(int dy=27;dy<=30;dy++){
+    printCandles(dy,mo,dow);
+  }
+  
+  //Dezember
+  mo=12;
+  for(int dy=1;dy<=24;dy++){
+    printCandles(dy,mo,dow);
+  }
+  
+	cout << endl;
+
+  return 0; 
+} 
